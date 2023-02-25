5f174f6024ae7d3a0797fce8f5c8ad2fab6e135b
You Song Hou <ysongweb3@gmail.com>
2023-02-24 21:10:30 -0500

    feat: add examples on Querying and Mutating the Blockchain

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,93 @@ import * as fcl from "@onflow/fcl";
 export default function Home() {
 
   const [user, setUser] = useState({loggedIn: null})
+  const [name, setName] = useState('')
+  const [newName, setNewName] = useState('')
+  const [transactionStatus, setTransactionStatus] = useState(null)
 
   useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+
+    setName(profile?.name ?? 'No Profile')
+  }
+
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+
+  // NEW
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg, t) => [arg(newName, t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
+  }
 
   const AuthedState = () => {
     return (
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <div>Transaction Status: {transactionStatus ?? "--"}</div>
+        <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button>
         <button onClick={fcl.unauthenticate}>Log Out</button>
+        <br />
+        <br />
+        <input placeholder='New Name' value={newName} onChange={(e) => setNewName(e.target.value)} />
+        <br />
+        <button onClick={executeTransaction}>Execute Transaction</button>
       </div>
     )
   }
@@ -40,5 +119,5 @@ export default function Home() {
         : <UnauthenticatedState />
       }
     </div>
-  );
+  )
 }
