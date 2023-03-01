c7f63eb0f7530f981ec99844929dda3f0b559767
You Song Hou <ysongweb3@gmail.com>
2023-02-28 20:44:55 -0500

    feat: load the images when the page load

--- src/pages/image-list.js
+++ src/pages/image-list.js
@@ -1,18 +1,19 @@
-import Head from 'next/head'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from 'next/router';
-import { Container, SimpleGrid, FormControl, FormLabel, Input, Image, Button } from '@chakra-ui/react';
+import { Container, SimpleGrid, Image, Button } from '@chakra-ui/react';
 import * as fcl from "@onflow/fcl";
 
 import "../../flow/config";
 
-export default function ImageList({ user }) {
+export default function ImageList() {
   const router = useRouter();
 
   const [urls, setURLs] = useState([])
-  const [newURL, setNewURL] = useState('')
-  const [transactionStatus, setTransactionStatus] = useState(null)
 
+  useEffect(() => {
+    sendQuery()
+  }, [])
+  
   const sendQuery = async () => {
     const result = await fcl.query({
       cadence: `
@@ -28,47 +29,16 @@ export default function ImageList({ user }) {
     setURLs(result)
   }
 
-  const AuthedState = () => {
-    return (
-      <div>
-        <div>Transaction Status: {transactionStatus ?? "--"}</div>
-        <Button onClick={sendQuery}>Send Query</Button>
-        <br />
-        <br />
-        <Button onClick={() => router.push("/add-image")}>
-          Add Image
-        </Button>
-        <br />
-        <br />
-        <SimpleGrid minChildWidth='200px' columns={[4]} spacing={10} mb='10'>
-          {urls.map((url, index) => (
-            <Image key={index} src={url} alt="Image" />
-          ))}
-        </SimpleGrid>
-      </div>
-    )
-  }
-
-  const UnauthenticatedState = () => {
-    return (
-      <div>
-        <p>Connect to Wallet</p>
-      </div>
-    )
-  }
-
   return (
     <Container maxW='1200px'>
-      <Head>
-        <title>Flow Image List App</title>
-        <meta name="description" content="Flow Image List App" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-        {user.loggedIn
-          ? <AuthedState />
-          : <UnauthenticatedState />
-        }
-     
+      <Button onClick={() => router.push("/add-image")} mb="5">
+        Add Image
+      </Button>
+      <SimpleGrid minChildWidth='200px' columns={[4]} spacing={10} mb='10'>
+        {urls.map((url, index) => (
+          <Image key={index} src={url} alt="Image" />
+        ))}
+      </SimpleGrid>
     </Container>
   )
 }
